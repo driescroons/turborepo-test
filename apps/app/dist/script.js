d17092ee31f3f9631902abaeb6399803f0678632
driescroons <dries.croons@panenco.com>
2022-03-14 20:27:30 +0100

    feat: adapting client to support seeds

--- apps/app/dist/script.js
+++ apps/app/dist/script.js
@@ -1,6 +1,10 @@
 const comments = [];
 const upvotes = [];
 
+// generate random seed for user currently using the page
+const seed = (Math.random() + 1).toString(36).substring(3);
+document.querySelector("#avatar").src = `https://i.pravatar.cc?u=${seed}`;
+
 const intervals = [
   { label: "year", seconds: 31536000 },
   { label: "month", seconds: 2592000 },
@@ -10,13 +14,19 @@ const intervals = [
   { label: "second", seconds: 1 },
 ];
 
+// formatting date for comment
 function timeSince(date) {
   const seconds = Math.floor((Date.now() - date.getTime()) / 1000);
   const interval = intervals.find((i) => i.seconds < seconds);
+  if (!interval) {
+    return "Just now";
+  }
+
   const count = Math.floor(seconds / interval.seconds);
   return `${count} ${interval.label}${count !== 1 ? "s" : ""} ago`;
 }
 
+// on page load, fetch comments
 (async () => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
 
@@ -40,8 +50,8 @@ const addComment = (comment) => {
   commentsDiv.insertAdjacentHTML(
     "beforeend",
     `<div class="flex flex-row gap-2">
-      <img src="https://ui-avatars.com/api/?name=${
-        comment.author
+      <img src="https://i.pravatar.cc?u=${
+        comment.seed
       }" alt="profile picture" class="rounded-full w-10 h-10" />
       <div class="ml-2">
         <div class="flex gap-2 items-center">
@@ -73,19 +83,36 @@ const addComment = (comment) => {
 };
 
 const postComment = async (e) => {
+  // make sure the page does not refresh
   e.preventDefault();
+
+  // get form data out of form
   const formData = new FormData(e.target);
   const formProps = Object.fromEntries(formData);
+
+  // check if text was inserted
+  if (!formProps.text) {
+    return;
+  }
+
   const comment = await (
     await fetch("/comments", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(formProps),
+      body: JSON.stringify({
+        ...formProps,
+        seed,
+      }),
     })
   ).json();
+
+  // push comment to local array
   comments.push(comment);
+  // reset the form
+  e.target.reset();
+  // render the comments (not really performant)
   renderComments();
 };
 
@@ -101,10 +128,15 @@ const upvoteComment = async (commentId) => {
     },
   });
 
+  // find the comment and upvote it client side (since 204)
   comments.find((comment) => comment.uuid === commentId).upvotes++;
+  // add the comment to the upvotes array
   upvotes.push(commentId);
+
+  // render the comments again (not really performant)
   renderComments();
 };
 
+// bind them to the window
 window.postComment = postComment;
 window.upvoteComment = upvoteComment;
