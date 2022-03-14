6831de2abc9af4ce940d02a56da2c0ae93f094b7
driescroons <dries.croons@panenco.com>
2022-03-14 19:45:55 +0100

    feat: adding list representation

---
+++ apps/api/src/utils/representer.ts
@@ -0,0 +1,28 @@
+import { ClassConstructor, plainToInstance } from "class-transformer";
+import { Action } from "routing-controllers";
+
+import { plainToClass } from "class-transformer";
+import {
+  createListRepresentation,
+  ListRepresentation,
+} from "../contracts/views/list.view";
+
+export const representer =
+  <T>(representationType: ClassConstructor<T>) =>
+  (_: Action, content: object) => {
+    return plainToInstance(representationType, content, {
+      exposeUnsetFields: false,
+    });
+  };
+
+export const listRepresenter =
+  <T>(representationType: ClassConstructor<T>) =>
+  (_: Action, content: [any[], number]) => {
+    const [items, count] = content;
+    const objects = items.map((item) => item.toJSON?.() ?? item);
+    const representation = new ListRepresentation(count, objects);
+    const listRepresentation = createListRepresentation(representationType);
+    return plainToInstance(listRepresentation, representation, {
+      exposeUnsetFields: false,
+    });
+  };
