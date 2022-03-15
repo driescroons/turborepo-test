e5e27b8002b4dcf59713e3317ac16cc3d0592d5a
driescroons <dries.croons@panenco.com>
2022-03-15 21:57:48 +0100

    feat: parent - child relation for comment

--- apps/api/src/entities/comment.entity.ts
+++ apps/api/src/entities/comment.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Property } from "@mikro-orm/core";
+import { Entity, ManyToOne, OneToMany, Property } from "@mikro-orm/core";
 import { Base } from "./base.entity";
 
 @Entity()
@@ -12,6 +12,12 @@ export class Comment extends Base {
   // used to select a random image and author name
   @Property()
   public seed: string;
+
+  @OneToMany(() => Comment, (comment) => comment.parent)
+  public children: Comment[] = [];
+
+  @ManyToOne(() => Comment, { nullable: true })
+  public parent?: Comment;
 
   @Property({ default: 0 })
   public upvotes: number = 0;
---
+++ apps/api/src/migrations/Migration20220315204943.ts
@@ -0,0 +1,16 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20220315204943 extends Migration {
+
+  async up(): Promise<void> {
+    this.addSql('alter table "comment" add column "parent_uuid" varchar(255) null;');
+    this.addSql('alter table "comment" add constraint "comment_parent_uuid_foreign" foreign key ("parent_uuid") references "comment" ("uuid") on update cascade on delete set null;');
+  }
+
+  async down(): Promise<void> {
+    this.addSql('alter table "comment" drop constraint "comment_parent_uuid_foreign";');
+
+    this.addSql('alter table "comment" drop column "parent_uuid";');
+  }
+
+}
