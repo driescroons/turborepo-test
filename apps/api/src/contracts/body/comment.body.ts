27c190b9b11bbe270ccf65546e70db03d152b7d5
driescroons <dries.croons@panenco.com>
2022-03-15 23:50:55 +0100

    feat: added validation

--- apps/api/src/contracts/body/comment.body.ts
+++ apps/api/src/contracts/body/comment.body.ts
@@ -1,10 +1,18 @@
 import { Exclude, Expose } from "class-transformer";
-import { IsOptional, IsString, IsUUID } from "class-validator";
+import {
+  IsOptional,
+  IsString,
+  IsUUID,
+  MaxLength,
+  MinLength,
+} from "class-validator";
 
 @Exclude()
 export class CommentBody {
   @Expose()
   @IsString()
+  @MinLength(2)
+  @MaxLength(255)
   public text: string;
 
   @Expose()
