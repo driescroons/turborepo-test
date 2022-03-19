18bc2ff7639078c53cc879f229c57514bf253ff2
driescroons <dries.croons@panenco.com>
2022-03-19 13:42:03 +0100

    fix: migration lint

--- apps/api/src/migrations/Migration20220310172739.ts
+++ apps/api/src/migrations/Migration20220310172739.ts
@@ -1,17 +1,23 @@
-import { Migration } from '@mikro-orm/migrations';
+import { Migration } from "@mikro-orm/migrations";
 
 export class Migration20220310172739 extends Migration {
-
   async up(): Promise<void> {
-    this.addSql('alter table "comment" drop constraint if exists "comment_upvotes_check";');
-    this.addSql('alter table "comment" alter column "upvotes" type int using ("upvotes"::int);');
+    this.addSql(
+      'alter table "comment" drop constraint if exists "comment_upvotes_check";'
+    );
+    this.addSql(
+      'alter table "comment" alter column "upvotes" type int using ("upvotes"::int);'
+    );
     this.addSql('alter table "comment" alter column "upvotes" set default 0;');
   }
 
   async down(): Promise<void> {
-    this.addSql('alter table "comment" drop constraint if exists "comment_upvotes_check";');
+    this.addSql(
+      'alter table "comment" drop constraint if exists "comment_upvotes_check";'
+    );
     this.addSql('alter table "comment" alter column "upvotes" drop default;');
-    this.addSql('alter table "comment" alter column "upvotes" type int using ("upvotes"::int);');
+    this.addSql(
+      'alter table "comment" alter column "upvotes" type int using ("upvotes"::int);'
+    );
   }
-
 }
